8fa9c4dfee68108e239f42cd041b59a9504b2b34
amalagoli79 <adriano.g.malagoli@gmail.com>
2022-11-29 21:36:04 -0300

    Eplayers-ER01-UC11--29-11-22

---
+++ Eplayers-Angular-UC11/src/app/views/login/login.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
+import { LoginService } from 'src/app/services/login.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(private loginService : LoginService, private router: Router) { }
+
+  ngOnInit(): void {
+  }
+    userModel = new User ();
+
+    mensagem = ""
+
+    receberDados() {this.loginService.login (this.userModel).subscribe((response) => {
+        alert("Logando*****") 
+        this.router.navigateByUrl("/")
+
+      }, (respostaErro) => {
+        // alert(respostaErro.error) 
+        // console.log(respostaErro.error) 
+        if (respostaErro.error == "Email and password are required") {
+          this.mensagem = "Email e senha são obrigatórios"
+        } else if (respostaErro.error == "Incorrect password") {
+          this.mensagem = "Senha Incorreta"
+        } else { 
+          this.mensagem = respostaErro.error 
+        }
+          
+
+      })
+
+    }
+}
+
+
+// console.log(this.userModel)
+
